a4642cccb1cf6bb5f15667e5746d4e1cff9f877c
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2016-04-24 19:20:01 -0300

    Comprador de carros consegue selectionar Marcas

---
+++ assets/javascript/main.js
@@ -0,0 +1,44 @@
+var DESVALORIZA = DESVALORIZA || {};
+
+DESVALORIZA.utilities = {};
+DESVALORIZA.utilities = {
+    sort_json: function (data) {
+        data.sort(function (maker_a, maker_b) {
+
+            if (maker_a.name > maker_b.name) {
+                return 1;
+            } else if (maker_a.name < maker_b.name) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+};
+
+DESVALORIZA.makers = {};
+DESVALORIZA.makers = {
+    select_id: '#maker',
+    fipe_url: 'http://fipeapi.appspot.com/api/1/carros/marcas.json',
+
+
+    install_makers: function () {
+        $.getJSON(DESVALORIZA.makers.fipe_url)
+            .done(function (data) {
+
+                DESVALORIZA.utilities.sort_json(data);
+
+                var maker_select = $(DESVALORIZA.makers.select_id);
+                $.each(data, function (i, item) {
+                    // console.log(item.id + item.name);
+                    maker_select.append($('<option></option>')
+                        .attr('value', item.id)
+                        .text(item.name));
+                })
+            });
+    }
+};
+
+$(document).ready(function () {
+    DESVALORIZA.makers.install_makers();
+});
