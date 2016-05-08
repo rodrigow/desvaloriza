cb74b837db58175c4c8e4e6c674c2a14ed663351
Thiago Spencer <Thiago Spencer>
2016-05-08 16:17:40 -0300

    Unit Testing.

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -3,3 +3,35 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
+
+describe("Testing utilities", function() {
+  var utilities;
+  
+  beforeEach(function() {
+    utilities = DESVALORIZA.utilities;
+  });
+
+  it("should be able to sort the combo", function() {
+	a = 'A';
+	b = 'B';
+	c = 'C';
+	d = 'D';
+	
+	combo = {
+				{"id": "4", "name": "D"},
+				{"id": "2", "name": "B"},
+				{"id": "3", "name": "C"},
+				{"id": "1", "name": "A"}
+			 };
+    
+	//utilities.sort_json(combo);
+    
+	//expect(utilities.name).toEqual(combo.name);
+	expect(null).toBeNull();
+    expect(a).toBeNull();
+    expect(foo).not.toBeNull();
+	expect(b).toMatch("B");
+
+  });
+
+});
