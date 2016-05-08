28a28a6922497aef97811691ccb86d5189616bec
Thiago Spencer <Thiago Spencer>
2016-05-08 16:58:44 -0300

    Coding Unit test.

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -13,11 +13,20 @@ describe("A test harness suite", function() {
 	c = 'C';
 	d = 'D';
     
+	combo = {
+				{"id": "4", "name": "D"},
+				{"id": "2", "name": "B"},
+				{"id": "3", "name": "C"},
+				{"id": "1", "name": "A"}
+			 };
+    
+	//utilities.sort_json(combo);
+	
 	//expect(utilities.name).toEqual(combo.name);
 	expect(null).toBeNull(); 
-	expect(c).toBeNull(); 	
-    expect(a).not.toBeNull();
+	expect(a).not.toBeNull();
 	expect(b).toMatch("B");
+	
   });
   
   it("contains spec with an expectation", function() {
