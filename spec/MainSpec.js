df5a77260196c5e781d3f3f0f7d5214833c0e930
Thiago Spencer <Thiago Spencer>
2016-05-08 16:30:35 -0300

    Fixing code.

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -10,26 +10,5 @@ describe("A suite", function() {
 	main = new Main();
   });
 
-  it("should be able to sort the combo", function() {
-	a = 'A';
-	b = 'B';
-	c = 'C';
-	d = 'D';
-	
-	combo = {
-				{"id": "4", "name": "D"},
-				{"id": "2", "name": "B"},
-				{"id": "3", "name": "C"},
-				{"id": "1", "name": "A"}
-			 };
-    
-	//utilities.sort_json(combo);
-    
-	//expect(utilities.name).toEqual(combo.name);
-	expect(null).toBeNull();
-    expect(a).toBeNull();
-    expect(foo).not.toBeNull();
-	expect(b).toMatch("B");
-
-  });
+  
 });
