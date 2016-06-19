7e1cfc57726dc2fe950656d17ea6a807b80ecce0
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2016-06-19 09:53:44 -0300

    cleans up code and removes unecessary code.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,4 @@
 var DESVALORIZA = DESVALORIZA || {};
-var MARKET_VALUE;
 
 DESVALORIZA.utilities = {};
 DESVALORIZA.utilities = {
@@ -14,41 +13,40 @@ DESVALORIZA.utilities = {
             }
         });
     },
-	addComboOptions:function (combo) {
-		combo.append($('<option></option>')
-                        .attr('value', '0')
-                        .text("Select the Model... "));
-	}
-	
+    addComboOptions: function (combo) {
+        combo.append($('<option></option>')
+            .attr('value', '0')
+            .text("Select the Model... "));
+    }
 };
 
 DESVALORIZA.models = {};
 DESVALORIZA.models = {
-	select_id: '#model',
+    select_id: '#model',
     fipe_url: 'http://fipeapi.appspot.com/api/1/carros/veiculos/id.json',
-	getModel: function () {
-	  var value = $(DESVALORIZA.makers.select_id).val();
-	  var url = DESVALORIZA.models.fipe_url.replace("id",value);
+    getModel: function () {
+        var value = $(DESVALORIZA.makers.select_id).val();
+        var url = DESVALORIZA.models.fipe_url.replace("id", value);
 
-	  $.getJSON(url)
+        $.getJSON(url)
             .done(function (data) {
                 DESVALORIZA.utilities.sort_json(data);
                 //console.log(DESVALORIZA.makers.select_id);
-			    var model_select = $(DESVALORIZA.models.select_id);
-				//console.log(DESVALORIZA.makers.select_id);
-                
-				model_select.append($('<option></option>')
-                        .attr('value', '0')
-                        .text("Select the Model... "));
-				
-				$.each(data, function (i, item) {
+                var model_select = $(DESVALORIZA.models.select_id);
+                //console.log(DESVALORIZA.makers.select_id);
+
+                model_select.append($('<option></option>')
+                    .attr('value', '0')
+                    .text("Select the Model... "));
+
+                $.each(data, function (i, item) {
                     console.log(item.id + item.name);
                     model_select.append($('<option></option>')
                         .attr('value', item.id)
                         .text(item.name));
-                })			
+                })
             });
-		}
+    }
 };
 
 DESVALORIZA.makers = {};
@@ -60,30 +58,30 @@ DESVALORIZA.makers = {
         $.getJSON(DESVALORIZA.makers.fipe_url)
             .done(function (data) {
                 DESVALORIZA.utilities.sort_json(data);
-               
-			    var maker_select = $(DESVALORIZA.makers.select_id);
-				//console.log(DESVALORIZA.makers.select_id);
-                
-				maker_select.append($('<option></option>')
-                        .attr('value', '0')
-                        .text("Select the Maker... "));
-				
-				$.each(data, function (i, item) {
+
+                var maker_select = $(DESVALORIZA.makers.select_id);
+                //console.log(DESVALORIZA.makers.select_id);
+
+                maker_select.append($('<option></option>')
+                    .attr('value', '0')
+                    .text("Select the Maker... "));
+
+                $.each(data, function (i, item) {
                     //console.log(item.id + item.name);
                     maker_select.append($('<option></option>')
                         .attr('value', item.id)
                         .text(item.name));
-                })	;		
+                });
             });
-		},
-		
-		onSelect: function () {
-			DESVALORIZA.models.getModel();
-		}
+    },
+
+    onSelect: function () {
+        DESVALORIZA.models.getModel();
+    }
 };
-	
+
 $(document).ready(function () {
     DESVALORIZA.makers.install();
-    
+
     DESVALORIZA.google.send_analytics();
 });
--- spec/UtilitiesSpec.js
+++ spec/UtilitiesSpec.js
@@ -1,35 +1,30 @@
-describe("utilities test", function() {
- 
-  it("should sort model data.", function() {
-	a = 'A';
-	b = 'B';
-	c = 'C';
-	d = 'D';
-    
-	data = {"Marcas":[
-				{"id": "4", "name": "D"},
-				{"id": "2", "name": "B"},
-				{"id": "3", "name": "C"},
-				{"id": "1", "name": "A"}
-			]};
-    
-	DESVALORIZA.utilities.sort_json(data.Marcas);
-		
-	expect(data).not.toBeNull();
-	expect(data.Marcas[0].name).toEqual("A");
-	expect(data.Marcas[1].name).toEqual("B");
-	expect(data.Marcas[2].name).toEqual("C");
-	expect(data.Marcas[3].name).toEqual("D");
-	
-	expect(data.Marcas[3].name).not.toEqual("A");
-	
-	
-  });
-  
-  it ("should append default option", function(){
-	  var combo = jasmine.createSpyObj("combo",["append"]);
-	  DESVALORIZA.utilities.addComboOptions(combo);
-	  expect(combo.append).toHaveBeenCalled();
-  });
-    
+describe("utilities test", function () {
+
+    it("should sort model data.", function () {
+        var data = {
+            "Marcas": [
+                {"id": "4", "name": "D"},
+                {"id": "2", "name": "B"},
+                {"id": "3", "name": "C"},
+                {"id": "1", "name": "A"}
+            ]
+        };
+
+        DESVALORIZA.utilities.sort_json(data.Marcas);
+
+        expect(data).not.toBeNull();
+        expect(data.Marcas[0].name).toEqual("A");
+        expect(data.Marcas[1].name).toEqual("B");
+        expect(data.Marcas[2].name).toEqual("C");
+        expect(data.Marcas[3].name).toEqual("D");
+
+        expect(data.Marcas[3].name).not.toEqual("A");
+    });
+
+    it("should append default option", function () {
+        var combo = jasmine.createSpyObj("combo", ["append"]);
+        DESVALORIZA.utilities.addComboOptions(combo);
+        expect(combo.append).toHaveBeenCalled();
+    });
+
 });
