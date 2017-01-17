93bdfe1cf0e923e44e51adfe24d7cdb7cd4d9fbb
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2017-01-16 17:17:42 -0800

    changes to local data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,19 +9,39 @@
   desvalorizaController.$inject = ['$http'];
   function desvalorizaController ($http) {
     var desvaloriza = this;
-    desvaloriza.type = 'carros';
-    desvaloriza.selected_maker = {};
-    desvaloriza.available_makers = [];
 
+    desvaloriza.type = 'carros';
+
+    desvaloriza.maker = {};
+    desvaloriza.available_makers = [];
     desvaloriza.loadMakers = function () {
       var url = 'http://fipeapi.appspot.com/api/1/TYPE/marcas.json';
-      $http.get(url.replace('TYPE', desvaloriza.type))
-        .then(
-          function(result) {
-            desvaloriza.available_makers = result.data;
-          });
+
+      // $http.get(url.replace('TYPE', desvaloriza.type))
+      //   .then(
+      //     function (result) {
+      //       desvaloriza.available_makers = result.data;
+      //     },
+      //     function (result) {
+      //       desvaloriza.serviceError = result;
+      //     });
+
+      var cars = [
+        {"name": "AUDI", "fipe_name": "Audi", "order": 2, "key": "audi-6", "id": 6},
+        {"name": "BMW", "fipe_name": "BMW", "order": 2, "key": "bmw-7", "id": 7}
+      ];
+      var bikes = [
+        {"name": "HONDA", "fipe_name": "Honda", "order": 2, "key": "honda-6", "id": 8},
+        {"name": "BMW", "fipe_name": "BMW", "order": 2, "key": "bmw-8", "id": 9}
+      ];
+      if (desvaloriza.type === 'carros') {
+        desvaloriza.available_makers = cars;
+      } else {
+        desvaloriza.available_makers = bikes;
+      }
     };
 
+    desvaloriza.model = {};
     desvaloriza.loadModels = function () {
 
     };
