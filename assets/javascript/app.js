a6de8247390668fd7363a3c8ac6c44b4950f011d
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2017-01-17 21:20:26 -0800

    shows models of a maker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,41 +5,58 @@
   var desvalorizaApp = angular.module('DesvalorizaApp', []);
   desvalorizaApp.controller('desvalorizaController', desvalorizaController);
   desvalorizaApp.service('makersService', makersService);
+  desvalorizaApp.service('modelsService', modelsService);
 
   var url = {
-    makers: 'https://fipe-parallelum.rhcloud.com/api/v1/TYPE/marcas'
+    makers: 'https://fipe-parallelum.rhcloud.com/api/v1/TYPE/marcas',
+    models: 'https://fipe-parallelum.rhcloud.com/api/v1/TYPE/marcas/MAKER/modelos'
   };
 
-  desvalorizaController.$inject = ['makersService'];
-  function desvalorizaController (makersService) {
+  desvalorizaController.$inject = ['makersService', 'modelsService'];
+  function desvalorizaController (makersService, modelsService) {
     var desvaloriza = this;
 
     desvaloriza.type = '';
 
     desvaloriza.maker = {};
     desvaloriza.available_makers = [];
-
-    var handleError = function (response) {
-      desvaloriza.error = response;
-    };
-
     desvaloriza.loadMakers = function () {
+      desvaloriza.available_models = [];
       makersService.fetch(desvaloriza.type).then(function (response) {
         desvaloriza.available_makers = response.data;
       }, handleError);
     };
 
     desvaloriza.model = {};
-    desvaloriza.loadModels = function () {};
+    desvaloriza.available_models = [];
+    desvaloriza.loadModels = function () {
+      if (desvaloriza.maker) {
+        modelsService.fetch(desvaloriza.type, desvaloriza.maker.codigo).then(function (response) {
+          desvaloriza.available_models = response.data.modelos;
+        }, handleError);
+      }
+    };
   };
 
   makersService.$inject = ['$http']
   function makersService($http) {
     return {
-      fetch: function(type) {
+      fetch: function (type) {
         return $http.get(url.makers.replace('TYPE', type));
       }
-    };
+    }
   };
 
+  modelsService.$inject = ['$http']
+  function modelsService($http) {
+    return {
+      fetch: function (type, maker) {
+        return $http.get(url.models.replace('TYPE', type).replace('MAKER', maker));
+      }
+    }
+  };
+
+  var handleError = function (response) {
+    desvaloriza.error = response;
+  };
 })();
